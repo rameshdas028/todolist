f64328e2fe0175086b5f1cc55f36b3f9be3eef1e
rameshdas028 <rameshdas028@gmail.com>
2020-05-22 17:39:49 +0530

    Updates

---
+++ precache-manifest.bfa692c118e63f4285f6b6730c49233b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d7cd7dab0a412db6ff790ea9427523",
+    "url": "/rameshdas028/todolist/index.html"
+  },
+  {
+    "revision": "3ddbbd26965bd30c36ea",
+    "url": "/rameshdas028/todolist/static/css/main.e6d1f80f.chunk.css"
+  },
+  {
+    "revision": "8309b474afa81553ee02",
+    "url": "/rameshdas028/todolist/static/js/2.0fe0cb80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rameshdas028/todolist/static/js/2.0fe0cb80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ddbbd26965bd30c36ea",
+    "url": "/rameshdas028/todolist/static/js/main.92e76177.chunk.js"
+  },
+  {
+    "revision": "5a42b61e2c4a749a11a6",
+    "url": "/rameshdas028/todolist/static/js/runtime-main.21661315.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rameshdas028/todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
